3ecfbb08960be036689776567a22a95a72b9e116
KHUSHI 444 <147485713+khushi4452@users.noreply.github.com>
2024-01-23 06:38:20 +0000

    added math.function to js

--- weath.js
+++ weath.js
@@ -1,8 +1,8 @@
 console.log(data);
 document.querySelector(".city").innerHtml = data.name;
-document.querySelector(".temp").innerHtml = data.main.temp + "c";
+document.querySelector(".temp").innerHtml = Math.round(data.main.temp)  + "c";
 document.querySelector(".humidity").innerHtml = data.main.humidity + "%";
-document.querySelector(".wind").innerHtml = data.wind.speed + "c";
+document.querySelector(".wind").innerHtml = data.wind.speed + "km/h";
 
 
 checkweather();
